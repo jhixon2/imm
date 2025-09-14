ef25407100a8849f18eeb83d0b78686e3fee0f1f
Janice Hixon <102612342+jhixon2@users.noreply.github.com>
2025-09-14 16:52:57 -0500

    faster image loading

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -5,5 +5,5 @@ export const client: SanityClient = createClient({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2023-01-01',
   token: process.env.SANITY_API_TOKEN,
-  useCdn: false,
+  useCdn: true,
 });
