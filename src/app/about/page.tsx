c05c8507986d5eb6da7185ac557d02c074df2a73
Janice Hixon <102612342+jhixon2@users.noreply.github.com>
2025-09-14 19:43:09 -0500

    Added description to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,12 +4,30 @@ import Image from 'next/image';
 import { urlFor } from '../../../lib/sanity.image';
 import Link from 'next/link';
 import { Photo } from '../../types';
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+const externalButtonStyle = {
+    marginTop: "1rem",
+    width: "10rem",
+    height: "2.5rem",
+    background: "none",
+    padding: "0",
+    border: "none",
+    cursor: "pointer",
+    fontSize: "16px",
+    display: "flex",
+    gap: "5px",
+    textDecoration: "underline",
+    alignItems: "center",
+  };
 
 export default async function AboutPage() {
   const profilePhoto: Photo = await client.fetch(`
     *[_type == "photo" && category == "ibi"][0]{
       _id,
       title,
+      description,
       mainImage{
         asset->{_id, url}
       }
@@ -19,24 +37,43 @@ export default async function AboutPage() {
     );
 
   return (
-    <main style={{ padding: '2rem', maxWidth: '600px', margin: 'auto', textAlign: 'center' }}>
+    <main style={{ padding: '2rem', maxWidth: '1000px', margin: 'auto', textAlign: 'center' }}>
       <h1>ABOUT</h1>
-      {profilePhoto?.mainImage?.asset?.url && (
-        <Image
-            src={urlFor(profilePhoto.mainImage).url()}
-            alt={profilePhoto.title}
-            width={1536}
-            height={2047}
-            style={{
-                display: 'block',
-                margin: '1rem auto',
-                width: '100%',
-                height: 'auto',
-                maxWidth: '400px',
-            }}
-          />
-      )}
-      {profilePhoto?.title && <p style={{ marginTop: '1rem' }}>{profilePhoto.title}</p>}
+      <div
+        style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "5rem",
+            margin: "1rem auto",
+          }}>
+        {profilePhoto?.mainImage?.asset?.url && (
+            <Image
+                src={urlFor(profilePhoto.mainImage).url()}
+                alt={profilePhoto.title}
+                width={1536}
+                height={2047}
+                style={{
+                    display: 'block',
+                    margin: '1rem auto',
+                    width: '100%',
+                    height: 'auto',
+                    maxWidth: '400px',
+                }}
+            />
+        )}
+        {profilePhoto?.description && (
+            <p style={{ maxWidth: "1536px", textAlign: "left", whiteSpace: "pre-line", fontSize: "20px", lineHeight: "32px"}}>
+            {profilePhoto.description}
+            <Link href="https://www.instagram.com/isabelmonikamarchand">
+            <button style={externalButtonStyle}><FaInstagram size={28} /> INSTAGRAM</button>
+            </Link>
+            <Link href="https://www.linkedin.com/in/isabel-marchand-45b885271">
+            <button style={externalButtonStyle}><FaLinkedin size={28} />  LINKEDIN</button>
+            </Link>
+            </p>
+        )}
+      </div>
       <Link
         href="/"
         style={{
